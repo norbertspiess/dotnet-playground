8e18c3f39d09818877690aee603cdbf758568157
Norbert Spiess <norbert.spiess@proton.me>
2025-06-24 15:18:25 +0200

    add examplary api validations

--- WebApplication1/Todo/Models/TodoItemDTO.cs
+++ WebApplication1/Todo/Models/TodoItemDTO.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApplication1.Todo.Models;
 
 public class TodoItemDTO
 {
     public long Id { get; set; }
+    [MaxLength(100)]
+    [RegularExpression("\\w+")]
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
 }
